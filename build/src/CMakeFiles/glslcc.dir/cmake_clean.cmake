ba95b72fa9fd0f815155777d8de1c6203e65fdb3
smartether <smartether@gmail.com>
2021-08-17 00:16:33 -0400

    new file:   src/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   src/CMakeFiles/glslcc.dir/DependInfo.cmake 	new file:   src/CMakeFiles/glslcc.dir/build.make 	new file:   src/CMakeFiles/glslcc.dir/cmake_clean.cmake 	new file:   src/CMakeFiles/glslcc.dir/depend.internal 	new file:   src/CMakeFiles/glslcc.dir/depend.make 	new file:   src/CMakeFiles/glslcc.dir/flags.make 	new file:   src/CMakeFiles/glslcc.dir/link.txt 	new file:   src/CMakeFiles/glslcc.dir/progress.make 	new file:   src/CMakeFiles/progress.marks 	new file:   src/Makefile 	new file:   src/cmake_install.cmake 	new file:   src/glslcc

---
+++ build/src/CMakeFiles/glslcc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glslcc.dir/config.cpp.o"
+  "CMakeFiles/glslcc.dir/glslcc.cpp.o"
+  "CMakeFiles/glslcc.dir/sgs-file.cpp.o"
+  "glslcc"
+  "glslcc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glslcc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
